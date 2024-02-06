072cc824a2ce1151009f3e498c88f24fe6c7c6b6
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-06 14:25:08 +0000

    enhancement: add books and display all books or random book

--- src/server.js
+++ src/server.js
@@ -3,7 +3,30 @@ const express = require("express");
 const port = 5001;
 const app = express();
 
-const home = "/"
+// HTTP Verbs - GET, POST, PUT, DELETE
+
+// const response = await fetch("https://example.com"); // sends GET request
+
+const home = "/listbooks"
+
+const bookList = [];
+
+app.use(express.json()); // Allows use of json data.
+
+app.get("/books", (request, response) => {
+    response.send({message: "success", books: bookList});
+});
+
+app.get("/books/getrandom", (request, response) => {
+    const randomIndex = Math.floor(Math.random() * bookList.length);
+    response.send(bookList[randomIndex]);
+});
+
+app.post("/books", (request, response) => {
+    for (item of request.body) bookList.push(item);
+
+    response.send({ message: "success", fakeArr: bookList });
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
