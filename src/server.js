8b89a54f1081a7d56fa09a38098d90cac08bd75f
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-06 09:37:13 +0000

    enhancement: initial console log links to homepage

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
+const port = 5001;
 const app = express();
 
-app.use("/example", express.static("example"));
+const home = "/express-site"
 
-app.listen(5001, () => {
-    console.log("Server is listening on port 5001");
+app.use("/example", express.static("example"));
+app.use(home, express.static("express-site"));
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    console.log(`Access at http://localhost:${port}${home}`);
 });
