6fcad06bb24e53e105c9c9926636751948647158
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-07 19:41:34 +0000

    feature: delete books from database

--- src/server.js
+++ src/server.js
@@ -11,29 +11,6 @@ const app = express();
 
 const home = "/books"
 
-const findMatching = (queries) => {
-    const returnList = []
-
-    console.log(queries);
-
-    for (const criteria of queries) {
-        bookList.map((item) => {
-            let matchingCriteria = 0;
-
-            // Compare the criteria against each book's properties.
-            for (const criterion in criteria) {
-                if (item[criterion] == criteria[criterion]) matchingCriteria++;
-                else break;
-            }
-
-            // If all critera match, add the book to the list
-            if (matchingCriteria == Object.keys(criteria).length) returnList.push(item);
-        });
-    }
-
-    return returnList;
-}
-
 const removeFromList = (items) => {
     for (const item of items) {
         const index = bookList.findIndex((book) => {
@@ -123,7 +100,13 @@ app.put("/books", (request, response) => {});
 
 // Delete
 
-app.delete("/books" , (request, response) => {});
+app.delete("/books", async (request, response) => {
+    const books = await Book.find(request.body);
+
+    await Book.deleteMany(request.body)
+
+    response.send({message: "Deletion successful", books:books});
+});
 
 
 app.listen(port, () => {
