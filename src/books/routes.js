9700b43c577d26b61e063fe89fac9e8b1de2d0d0
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-08 15:08:39 +0000

    refactor: moved functions from routes.js to controllers.js

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,69 +1,31 @@
 const { Router } = require("express");
 const bookRouter = Router();
+const {addBooks, getBooks, getRandomBook, findBooks, updateBooks, removeBooks} = require("./controllers");
 
 const Book = require("./model");
 
 // Create
 
-bookRouter.post("/books", async (request, response) => {
-    const successful = [];
-
-    for (item of request.body) {
-        try {
-            const book = new Book(item);
-            await book.save();
-            successful.push(book);
-        } catch (err) {
-            console.error(item.title + " already in database");
-        }
-    }
-
-    response.send({ message: `${successful.length}/${request.body.length} items added to database.`, books:successful });
-})
+bookRouter.post("/books", addBooks)
 
 
 // Read
 
-bookRouter.get("/books", async (request, response) => {
-    const books = await Book.find({});
+bookRouter.get("/books", getBooks);
 
-    response.send(books);
-});
+bookRouter.get("/books/getrandom", getRandomBook);
 
-bookRouter.get("/books/getrandom", async (request, response) => {
-    const books = await Book.find({});
-    const randomInt = Math.floor(Math.random() * books.length); 
-
-    response.send(books[randomInt]);
-});
-
-bookRouter.get("/books/search", async (request, response) => {
-    const books = await Book.find(request.body);
-
-    response.send(books);
-});
+bookRouter.get("/books/search", findBooks);
 
 
 // Update
 
-bookRouter.put("/books", async (request, response) => {
-    const targets = await Book.find(request.body.target);
-
-    await Book.updateMany(request.body.target, request.body.correction);
-
-    response.send({message: "Update successful", targets:targets});
-});
+bookRouter.put("/books", updateBooks);
 
 
 // Delete
 
-bookRouter.delete("/books", async (request, response) => {
-    const books = await Book.find(request.body);
-
-    await Book.deleteMany(request.body)
-
-    response.send({message: "Deletion successful", books:books});
-});
+bookRouter.delete("/books", removeBooks);
 
 
 module.exports = bookRouter;
