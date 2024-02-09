54a803f1fbaf053a13b78433462f2fcc02c3c352
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-09 10:54:20 +0000

    refactor: move book controller functions into methods.export

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -3,92 +3,81 @@ const Book = require("./model");
 
 // Create
 
-const addBooks = async (request, response) => {
-    const successful = [];
-
-    for (item of request.body) {
-        try {
-            const book = new Book(item);
-            await book.save();
-            successful.push(book);
-        } catch (err) {
-            console.error(item.title + " already in database");
-        }
-    }
-
-    response.send({ message: `${successful.length}/${request.body.length} items added to database.`, books:successful });
-}
-
-
-// Read
-
-const getBooks = async (request, response) => {
-    const books = await Book.find({});
-
-    response.send(books);
-}
-
-const getRandomBook = async (request, response) => {
-    const books = await Book.find({});
-    const randomInt = Math.floor(Math.random() * books.length); 
-
-    response.send(books[randomInt]);
-}
-
-const findBooks = async (request, response) => {
-    const query = {}
-
-    // 
-    query[request.params["property"]] = {
-        '$regex': request.params["value"],
-         $options: 'is' 
-        }
-
-    console.log(request.params["property"])
-    console.log(query);
-
-    const books = await Book.find(query);
-
-    response.send(books);
-}
-
-const listValues = async (request, response) => {
-    response.send(
-        Book.distinct(request.params["property"])
-    )
-}
-
-
-// Update
-
-const updateBooks = async (request, response) => {
-    const targets = await Book.find(request.body.target);
-
-    await Book.updateMany(request.body.target, request.body.correction);
-
-    response.send({message: "Update successful", targets:targets});
-}
-
-
-// Delete
-
-const removeBooks = async (request, response) => {
-    const books = await Book.find(request.body);
-
-    await Book.deleteMany(request.body)
-
-    response.send({message: "Deletion successful", books:books});
-}
-
-
-// const addBook = async (request, response) => {
-
 module.exports = {
-    addBooks: addBooks,
-    getBooks: getBooks,
-    getRandomBook: getRandomBook,
-    findBooks: findBooks,
-    updateBooks: updateBooks,
-    removeBooks: removeBooks,
-    listValues: listValues,
+    addBooks: async (request, response) => {
+        const successful = [];
+
+        for (item of request.body) {
+            try {
+                const book = new Book(item);
+                await book.save();
+                successful.push(book);
+            } catch (err) {
+                console.error(item.title + " already in database");
+            }
+        }
+
+        response.send({ message: `${successful.length}/${request.body.length} items added to database.`, books:successful });
+    },
+
+
+    // Read
+
+    getBooks: async (request, response) => {
+        const books = await Book.find({});
+
+        response.send(books);
+    },
+
+    getRandomBook: async (request, response) => {
+        const books = await Book.find({});
+        const randomInt = Math.floor(Math.random() * books.length); 
+
+        response.send(books[randomInt]);
+    },
+
+    findBooks: async (request, response) => {
+        const query = {}
+
+        // 
+        query[request.params["property"]] = {
+            '$regex': request.params["value"],
+            $options: 'is' 
+            }
+
+        console.log(request.params["property"])
+        console.log(query);
+
+        const books = await Book.find(query);
+
+        response.send(books);
+    },
+
+    listValues: async (request, response) => {
+        response.send(
+            Book.distinct(request.params["property"])
+        )
+    },
+
+
+    // Update
+
+    updateBooks: async (request, response) => {
+        const targets = await Book.find(request.body.target);
+
+        await Book.updateMany(request.body.target, request.body.correction);
+
+        response.send({message: "Update successful", targets:targets});
+    },
+
+
+    // Delete
+
+    removeBooks: async (request, response) => {
+        const books = await Book.find(request.body);
+
+        await Book.deleteMany(request.body)
+
+        response.send({message: "Deletion successful", books:books});
+    },
 }
